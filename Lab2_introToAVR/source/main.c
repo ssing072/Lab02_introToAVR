66b9eb27e5d538fbae1ea51ee5963b95431745ab
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-03 11:59:13 -0700

    Update "Reading from input pins"

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -12,11 +12,16 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void){
-	DDRB = 0xFF; // Configure port B's 8 pins as outputs
-	PORTB = 0x00; // Initialize PORTB output to 0’s
-	while(1){
-		PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+					// Initialize output on PORTB to 0x00
+	unsigned char temp_input = 0x00;
+	while(1) {
+		temp_input = PINA;
+		PORTB = temp_input;	// Writes port B's 8 pins with the values
+					// on port A's 8 pins
 	}
 	return 1;
 }
+
