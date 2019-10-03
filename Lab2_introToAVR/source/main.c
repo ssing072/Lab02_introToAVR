402dbf276cdd39e5c2ff33e4a552047b9098ec4d
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-03 11:54:57 -0700

    "Set port  B 8 Pins"

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -12,12 +12,11 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
+int main(void){
+	DDRB = 0xFF; // Configure port B's 8 pins as outputs
+	PORTB = 0x00; // Initialize PORTB output to 0’s
+	while(1){
+		PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+	}
+	return 1;
 }
