19303e63e75aece08328ccbad18c67bd76d445fe
ssing072 <36577991+ssing072@users.noreply.github.com>
2019-10-03 12:34:45 -0700

    test

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -11,6 +11,7 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+/*
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
@@ -21,17 +22,41 @@ int main(void) {
 		tmpA = PINA & 0x01;
 		// 2) Perform computation
 		// if PA0 is 1, set PB1PB0 = 01, else = 10
-		if (tmpA == 0x01) { // True if PA0 is 1
+		if (tmpA == 0x01) { // True if  PA0 is 1
 			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
 							 // (clear rightmost 2 bits, then set to 01)
 		} else {
-			tmpB = (tmpB & 0xFC) | 0x00; // Sets tmpB to bbbbbb10
-							 // (clear rightmost 2 bits, then set to 10)
+			tmpB = (tmpB & 0xFC) | 0x00; // Sets tmpB to bbbbbb00
+							 // (clear rightmost 2 bits, then set to 00)
 		}	
 	// 3) Write output
 	PORTB = tmpB;	
 	}
 	return 0;
 }
-
-
+*/
+int main(void) {
+    DDRA = 0x00;
+    PORTA = 0xFF;
+    DDRB = 0xFF;
+    PORTB = 0x00;
+    unsigned char tmp1 = 0x00;
+    unsigned char tmp2 = 0x00;
+    unsigned char tmp3 = 0x00;
+    while (1)
+    {
+    tmp1 = PINA & 0x01;
+    tmp2 = PINA & 0x02;
+    if( tmp1 == 0x01 && tmp2 == 0x00)
+    {
+    tmp3 = (tmp3 & 0xFE) | 0x01;
+    }
+    else
+    {
+    tmp3 = (tmp3 & 0xFE) | 0x00;
+    }
+    PORTB = tmp3;
+   }
+ // }
+  return 1;
+ }
